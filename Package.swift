fc054b3f91b963eff53510cf24e3a00d81a1239c
Jeongyegean <wjddpwls12@naver.com>
2019-10-31 10:48:24 +0900

    f

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             name: "eunziTest",
             path: "Sources",
             exclude: [],
-            sources: ["eunziTest"],
+            sources: ["eunziTest/RESideMenu.m"],
             publicHeadersPath: "eunziTest"
         ),
         .testTarget(
